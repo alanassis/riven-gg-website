eea8f57e3be212b9c232bf6141d6e0e85730af8f
Alan Assis <alanpenteado.work@gmail.com>
2021-02-24 17:23:43 -0300

    fix: renomeia name para nickname

--- src/services/profileRepository.js
+++ src/services/profileRepository.js
@@ -3,7 +3,9 @@ import repository from "./repository";
 const resource = "/profile";
 
 export default {
-  getSummoner(summonerName) {
-    return repository.get(`${resource}/${summonerName}`);
+  getSummoner(searchData) {
+    return repository.get(
+      `${resource}/${searchData.region}/${searchData.nickname}`
+    );
   },
 };
