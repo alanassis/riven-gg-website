5459eadddd5cd95e17907711b0bd47a5a2d36dbb
Alan Assis <alanpenteado.work@gmail.com>
2021-02-24 01:30:26 -0300

    improve: altera a altura do background para tela

---
+++ src/components/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-img :height="screenHeight" src="@/assets/riven-0.jpg" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+
+  data: () => ({}),
+
+  computed: {
+    screenHeight() {
+      return this.$vuetify.breakpoint.height;
+    },
+  },
+};
+</script>
