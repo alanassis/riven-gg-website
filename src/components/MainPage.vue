aee5cfe85df561777a776baed3f7d48ef4a1c9f5
Alan Assis <alanpenteado.work@gmail.com>
2021-02-27 04:58:34 -0300

    style: ajusta card, imagens dos elos e histórico

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,7 +4,7 @@
       <v-container fill-height v-if="summoner">
         <v-row justify="center" align="center">
           <v-card
-            width="75vmin"
+            width="80vmin"
             height="90vmin"
             class="pa-4"
             color="rgb(255, 255, 255, 0.75)"
@@ -14,27 +14,45 @@
                 <span v-show="leagues.length > 0">
                   <div class="d-flex justify-center">
                     <v-avatar size="17vmin">
-                      <v-img src="@/assets/dawnbringer-logo.png" />
+                      <v-img
+                        v-if="soloDuoLeague.tier !== 'UNRANKED'"
+                        :src="
+                          require(`@/assets/ranked-emblems/${soloDuoLeague.tier}.png`)
+                        "
+                      />
                     </v-avatar>
                   </div>
-                  <v-card-title class="text-subtitle-2 justify-center mt-n4">{{
+                  <v-card-title class="text-subtitle-2 justify-center mt-n2">{{
                     `${soloDuoLeague.tier} ${soloDuoLeague.rank}`
                   }}</v-card-title>
+                  <v-card-title class="text-caption justify-center mt-n8"
+                    >SOLO/DUO</v-card-title
+                  >
                 </span>
               </v-col>
               <v-avatar size="30vmin">
-                <v-img :src="summoner.profileIcon" />
+                <v-img
+                  :src="`http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${summoner.profileIconId}.png`"
+                />
               </v-avatar>
               <v-col>
                 <span v-show="leagues.length > 0">
                   <div class="d-flex justify-center">
                     <v-avatar size="17vmin">
-                      <v-img src="@/assets/dawnbringer-logo.png" />
+                      <v-img
+                        v-if="flexLeague.tier !== 'UNRANKED'"
+                        :src="
+                          require(`@/assets/ranked-emblems/${flexLeague.tier}.png`)
+                        "
+                      />
                     </v-avatar>
                   </div>
-                  <v-card-title class="text-subtitle-2 justify-center mt-n4">{{
+                  <v-card-title class="text-subtitle-2 justify-center mt-n2">{{
                     `${flexLeague.tier} ${flexLeague.rank}`
                   }}</v-card-title>
+                  <v-card-title class="text-caption justify-center mt-n8"
+                    >FLEX</v-card-title
+                  >
                 </span>
               </v-col>
               <v-col class="text-center" cols="12">
@@ -52,16 +70,18 @@
                     <template v-slot:default="{ item }">
                       <v-list-item :key="item.gameId">
                         <v-list-item-action class="mr-2">
-                          <v-btn fab small depressed color="primary">
-                            {{ item.summonerParticipant.championName[0] }}
-                          </v-btn>
+                          <v-avatar>
+                            <v-img
+                              :src="`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${item.summonerParticipant.championName}.png`"
+                            />
+                          </v-avatar>
                         </v-list-item-action>
 
-                        <v-list-item-content>
+                        <!-- <v-list-item-content>
                           <v-list-item-title class="text-subtitle-2">
                             {{ item.summonerParticipant.championName }}
                           </v-list-item-title>
-                        </v-list-item-content>
+                        </v-list-item-content> -->
 
                         <v-list-item-content>
                           <v-list-item-title class="text-center">
@@ -87,16 +107,18 @@
                           </v-list-item-title>
                         </v-list-item-content>
 
-                        <v-list-item-content>
+                        <!-- <v-list-item-content>
                           <v-list-item-title class="text-subtitle-2 text-end">
                             {{ item.enemyParticipant.championName }}
                           </v-list-item-title>
-                        </v-list-item-content>
+                        </v-list-item-content> -->
 
                         <v-list-item-action class="ml-2">
-                          <v-btn fab small depressed color="primary">
-                            {{ item.enemyParticipant.championName[0] }}
-                          </v-btn>
+                          <v-avatar>
+                            <v-img
+                              :src="`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${item.enemyParticipant.championName}.png`"
+                            />
+                          </v-avatar>
                         </v-list-item-action>
                       </v-list-item>
                     </template>
@@ -123,6 +145,7 @@ export default {
   }),
 
   props: {
+    version: String,
     summoner: Object,
   },
 
@@ -169,7 +192,7 @@ export default {
     },
     soloDuoLeague() {
       for (let i of this.leagues) {
-        if (i.queueType === "RANKED_SOLO_SR") return i;
+        if (i.queueType === "RANKED_SOLO_5x5") return i;
       }
       return { tier: "UNRANKED", rank: "" };
     },
